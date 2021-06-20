54ebc9ecdee98c37463a1ca54fd86b145d9827f8
Andriy Lesyuk <s-andy@andriylesyuk.com>
2021-06-20 13:51:56 +0300

    Support threaded view for Redmine 4.1

--- lib/that_thread_hooks.rb
+++ lib/that_thread_hooks.rb
@@ -4,6 +4,12 @@ class ThatThreadHook  < Redmine::Hook::ViewListener
         stylesheet_link_tag('threads', :plugin => 'that_thread')
     end
 
+    def view_issues_history_journal_bottom(context = {})
+        if Setting.plugin_that_thread['display'] == 'thread' && !User.current.wants_comments_in_reverse_order?
+            context[:hook_caller].javascript_tag("$('#change-#{context[:journal].id}')[0].style.setProperty('--indice', #{context[:journal].indice});")
+        end
+    end
+
     render_on :view_issues_show_details_bottom, partial: 'issues/thread'
     render_on :view_issues_edit_notes_bottom, :partial => 'issues/reply_to'
 
